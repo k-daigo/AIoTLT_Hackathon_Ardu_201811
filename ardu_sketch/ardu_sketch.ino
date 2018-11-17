0837cf5be31f2722e15f2ab3ee059877e5bc7e7e
k-daigo <k-daigo@kt.rim.or.jp>
2018-11-17 18:50:19 +0900

    faet: メインの骨組み

--- ardu_sketch/ardu_sketch.ino
+++ ardu_sketch/ardu_sketch.ino
@@ -1,11 +1,67 @@
+#include "ML.h"
 #include "Rireki.h"
+#include "Sound.h"
 
-void setup() {
+#define PROC_INTERVAL (30 * 60 * 1000)
+#define VOICE_KBN_GOOD 0
+#define VOICE_KBN_ALTER 1
+
+// 経過時間
+//unsigned long time;
+
+// ボイス区分
+int voice_kbn = VOICE_KBN_GOOD;
+int prev_voice_kbn = VOICE_KBN_GOOD;
+
+void setup()
+{
   // put your setup code here, to run once:
-
 }
 
-void loop() {
+void loop()
+{
+  // 一定時間Sleep
+  delay(PROC_INTERVAL);
+
+  // 人がいるか判定（動体検出）
+  boolean hito_iru = false;
+  if (hito_iru)
+  {
+    if (voice_kbn == VOICE_KBN_ALTER)
+    {
+      // ボイス発話（アラート）
+    }
+    else
+    {
+      // ボイス発話（褒める）
+    }
+    return;
+  }
+
+  // 人がいない処理
+  // 画像撮影
+
+  // 推論実行
+
+  // 履歴保存
   SaveRireki();
-  
+
+  // 汚部屋度判定
+  if (prev_voice_kbn != VOICE_KBN_ALTER)
+  {
+    // 汚部屋閾値以上
+    if (true)
+    {
+      voice_kbn = VOICE_KBN_ALTER;
+
+      // インスタ投稿
+      // NOP
+    }
+  }
+  else
+  {
+    // 汚部屋閾値未満
+      voice_kbn = VOICE_KBN_GOOD;
+    
+  }
 }
